7e3eba5662c4a43d320b03e6208314dda9ae1dc8
messiasloiola <messiasloiolas@gmail.com>
2025-10-10 14:58:35 -0300

    style: Apply ESLint auto-fixes

--- src/models/post.js
+++ src/models/post.js
@@ -5,30 +5,30 @@ const strToBool = (value) => {
   return value === 'true';
 };
 
-const DB_DATABASE = process.env.DB_DATABASE || "kubedevnews";
-const DB_USERNAME = process.env.DB_USERNAME || "kubedevnews";
-const DB_PASSWORD = process.env.DB_PASSWORD || "Pg#123";
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_DATABASE = process.env.DB_DATABASE || 'kubedevnews';
+const DB_USERNAME = process.env.DB_USERNAME || 'kubedevnews';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Pg#123';
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = parseInt(process.env.DB_PORT, 10) || 5432; 
 const DB_SSL_REQUIRE =  strToBool(process.env.DB_SSL_REQUIRE) || false;
 
 const seque = new sequelize.Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: DB_SSL_REQUIRE ? {
-        require: true,
-        rejectUnauthorized: false
-      } : false
-    }
-  });
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: DB_SSL_REQUIRE ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  }
+});
 
 class Post extends sequelize.Model {
   
   save() {
     
-    console.log('Entrou')
+    console.log('Entrou');
     super.save();
   }
 }
@@ -53,11 +53,11 @@ Post.init({
 }, {
   sequelize: seque, // We need to pass the connection instance
   modelName: 'Post' // We need to choose the model name
-})
+});
 
 exports.initDatabase = () => {
-    seque.sync({ alter: true })
-}
+  seque.sync({ alter: true });
+};
 
 exports.Post = Post;
 
--- src/server.js
+++ src/server.js
@@ -1,80 +1,80 @@
 const express = require('express');
 const app = express();
-const models = require('./models/post')
-const bodyParser = require('body-parser')
-const promBundle = require("express-prom-bundle");
+const models = require('./models/post');
+const bodyParser = require('body-parser');
+const promBundle = require('express-prom-bundle');
 const config = require('./system-life');
-const middlewares = require('./middleware')
+const middlewares = require('./middleware');
 
 const metricsMiddleware = promBundle({
-    includeMethod: true, 
-    includePath: true, 
-    includeStatusCode: true, 
-    includeUp: true,
-    promClient: {
-        collectDefaultMetrics: {
-        }
-      }
+  includeMethod: true, 
+  includePath: true, 
+  includeStatusCode: true, 
+  includeUp: true,
+  promClient: {
+    collectDefaultMetrics: {
+    }
+  }
 });
 
-app.use(middlewares.countRequests)
-app.use(metricsMiddleware)
+app.use(middlewares.countRequests);
+app.use(metricsMiddleware);
 app.use(config.middlewares.healthMid);
 app.use('/', config.routers);
 app.use(express.static('static'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 
 app.get('/post', (req, res) => {
-    res.render('edit-news', {post: {title: "", content: "", summary: ""}, valido: true});
+  res.render('edit-news', {post: {title: '', content: '', summary: ''}, valido: true});
 });
 
 app.post('/post', async (req, res) => {
 
-    let valid = true;
+  let valid = true;
 
-    if ((req.body.title.length !== 0 && req.body.title.length < 30) && 
+  if ((req.body.title.length !== 0 && req.body.title.length < 30) && 
         (req.body.resumo.length !== 0 && req.body.resumo.length < 50) &&
         (req.body.description.length !== 0 && req.body.description.length < 2000)) {
-        valid = true;
-    } else {
-        valid = false;
-    }
+    valid = true;
+  } else {
+    valid = false;
+  }
 
-    if (valid) {
-        await models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
-        res.redirect('/');
-    } else {
-        res.render('edit-news', {post: {title: req.body.title, content: req.body.description, summary: req.body.resumo}, valido: false});
-    }
+  if (valid) {
+    await models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
+    res.redirect('/');
+  } else {
+    res.render('edit-news', {post: {title: req.body.title, content: req.body.description, summary: req.body.resumo}, valido: false});
+  }
     
 });
 
 app.post('/api/post', async (req, res) => {
 
-    console.log(req.body.artigos)
-    for(const item of req.body.artigos) {
+  console.log(req.body.artigos);
+  for(const item of req.body.artigos) {
 
-        await models.Post.create({title: item.title, content: item.description, summary: item.resumo, publishDate: Date.now()});
-    }
+    await models.Post.create({title: item.title, content: item.description, summary: item.resumo, publishDate: Date.now()});
+  }
 
-    // models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
-    res.json(req.body.artigos)
+  // models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
+  res.json(req.body.artigos);
 });
 
 app.get('/post/:id', async (req, res) => {
 
-    const post = await models.Post.findByPk(req.params.id);
-    res.render('view-news',{post: post});
+  const post = await models.Post.findByPk(req.params.id);
+  res.render('view-news',{post: post});
 });
 
 
 app.get('/', async (req, res) => {
 
-    const posts = await models.Post.findAll();
-    res.render('index',{posts: posts});
+  const posts = await models.Post.findAll();
+  res.render('index',{posts: posts});
 });
 
 models.initDatabase();
